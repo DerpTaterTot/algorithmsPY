fd609de2521a73064c8646bdc1c5632588785224
Albert Chen <chenalbert06@gmail.com>
2021-02-16 12:46:38 -0500

    binary converter

---
+++ binary.py
@@ -0,0 +1,23 @@
+from stack import Stack
+
+stack = Stack()
+
+def convert_int_to_bin(num):
+    while num > 0:              
+        stack.push(num % 2)
+
+        num //= 2  
+    
+    number = 0
+    
+    print(stack.get_stack())
+    
+    for i in range(len(stack.get_stack())):
+        number += stack.get_stack()[i] * 10 ** i
+    
+    return number
+
+print(convert_int_to_bin(12))
+        
+
+       
